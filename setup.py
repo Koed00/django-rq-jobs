eaa045ff371374b7a73495e68202fce430bcf115
Ilan Steemers <koed00@gmail.com>
2015-05-07 15:41:10 +0200

    New pypi release 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 setup(
     name='django-rq-jobs',
-    version='0.1.0',
+    version='0.1.1',
     author='Ilan Steemers',
     author_email='koed00@gmail.com',
     packages=['django_rq_jobs'],
