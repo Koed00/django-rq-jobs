8dd7557ca675fba3c15e2c48a42ccf29592993bf
Ilan <koed00@gmail.com>
2015-07-29 12:16:53 +0200

    v0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rq-jobs',
-    version='0.1.4',
+    version='0.1.5',
     author='Ilan Steemers',
     author_email='koed00@gmail.com',
     packages=['django_rq_jobs'],
